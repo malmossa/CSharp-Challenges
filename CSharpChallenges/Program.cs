36fa3ab50f1847915e31ab718bf9f0d30b77ce55
Mansor Almossa <malmossa@gmail.com>
2024-12-08 15:12:35 -0800

    completed

---
+++ CSharpChallenges/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CShapChallenges
+{
+  public class Program
+  {
+    static void Main(string[] args)
+    {
+      int result = SumOfTwo_Challenge.SumOfTwo(6, 5);
+      Console.WriteLine(result);
+    }
+  }
+
+}
